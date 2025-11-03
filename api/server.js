408f4b9dadb6cad47f5beec0ad58663fd2f53aec
Harshit-jain-57 <jainharshit877@gmail.com>
2025-11-04 00:42:43 +0530

    added the env frontendurl

--- api/server.js
+++ api/server.js
@@ -25,7 +25,7 @@ const connect =async()=>{
     }
 }
 //middlewars
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:"https://freelance-market-place-h65a-c1z04q6i9.vercel.app/",credentials:true}))
 app.use(express.json())
 app.use(cookieParser())
 
